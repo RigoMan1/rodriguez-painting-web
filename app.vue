86f809c1534dd3c67dbe327d3d2a1843f5e2800f
RigoMan1 <rodrigoalonso3781@gmail.com>
2025-04-26 12:44:16 -0500

    feat: add SEO metadata and Open Graph image for improved sharing

--- app.vue
+++ app.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const domain = 'rodriguez-painting-web.vercel.app';
+const title = 'Rodriguez - Painting & Striping Services';
+const description =
+  'Rodriguez Painting & Striping Services - Your trusted partner for all your painting and striping needs. Contact us today for a free estimate!';
+const image = `https://${domain}/images/og-image-main.jpg`;
+
+useSeoMeta({
+  ogTitle: title,
+  ogDescription: description,
+  ogImage: image,
+  // ogUrl: `https://${domain}`,
+  ogType: 'website',
+});
+</script>
 
 <template>
   <v-app class="bg-gray-50">
